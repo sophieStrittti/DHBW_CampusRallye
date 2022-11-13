39f7b1405c7acbe7f177aeb8e2a0a68cc2818e2c
sophieStritti <sophie.strittmatter@gmail.com>
2022-11-13 21:18:44 +0100

    test

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/*style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}*/}
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" backgroundColor='#ff0000' />
+      <Text>Test Text</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
