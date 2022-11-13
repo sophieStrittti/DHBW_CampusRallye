547505db8e6c77e04979c955ad52be8f52ae839d
sophieStritti <sophie.strittmatter@gmail.com>
2022-11-13 22:31:26 +0100

    tests

--- App.js
+++ App.js
@@ -1,26 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      {/*style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}*/}
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" backgroundColor='#ff0000' />
-      <Text>Hier entsteht eine Campus Ralley App der DHBW Lörrach</Text>
+      <View style={styles.contentView}>
+        <Text style={styles.ownText}>Hiermit akzeptiere ich, dass Bildmaterial von mir gespeichert und auch bei der Ergebnisrunde gezeigt werden darf (oder so ähnlich)</Text>
+       
+        <Button title='Bestätigen'/>
+      </View>
     </View>
+    /* View ist ein Container, nur eine View in einer Komponente, aber mehrere Unterviews/Views in der View */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  contentView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'stretch',
+    
+  },
+
+  ownText: {
+    backgroundColor: 'grey',
+    with: 200,
+  }
 });
