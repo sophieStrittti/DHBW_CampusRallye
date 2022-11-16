a998a069dab3e95861b804b6648d53c5e173c7f3
sophieStritti <sophie.strittmatter@gmail.com>
2022-11-16 14:37:44 +0100

    switch without action added

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Switch} from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    
     <View style={styles.container}>
       <StatusBar style="auto" backgroundColor='#ff0000' />
       <View style={styles.contentView}>
         <Text style={styles.ownText}>Hiermit akzeptiere ich, dass Bildmaterial von mir gespeichert und auch bei der Ergebnisrunde gezeigt werden darf (oder so ähnlich)</Text>
-       
+        <View style = {styles.radiocontainer}>
+        <Switch
+          trackColor={{ false: "grey", true: "red" }} 
+          onValueChange={toggleSwitch} 
+          value={isEnabled}
+          thumbColor={"#f4f3f4"}
+        />
+          <Text style={styles.ownText}>Ich akzeptiere.</Text>
+        </View>
         <Button title='Bestätigen'/>
       </View>
     </View>
@@ -36,5 +47,12 @@ const styles = StyleSheet.create({
   ownText: {
     color: 'grey',
     fontSize: 25,
+  
+  },
+
+  radiocontainer:{
+    flexDirection: 'row',
+    justifyContent: 'center',
   }
+
 });
