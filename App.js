51ed6ad6fe7902a03758aa67d4c6f8218d83838d
sophieStritti <sophie.strittmatter@gmail.com>
2022-11-16 14:50:16 +0100

    changed button style

--- App.js
+++ App.js
@@ -8,19 +8,18 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" backgroundColor='#ff0000' />
       <View style={styles.contentView}>
         <Text style={styles.ownText}>Hiermit akzeptiere ich, dass Bildmaterial von mir gespeichert und auch bei der Ergebnisrunde gezeigt werden darf (oder so ähnlich)</Text>
         <View style = {styles.radiocontainer}>
-        <Switch
-          trackColor={{ false: "grey", true: "red" }} 
-          onValueChange={toggleSwitch} 
-          value={isEnabled}
-          thumbColor={"#f4f3f4"}
-        />
+          <Switch
+            trackColor={{ false: "grey", true: "red" }} 
+            onValueChange={toggleSwitch} 
+            value={isEnabled}
+            thumbColor={"#f4f3f4"}
+          />
           <Text style={styles.ownText}>Ich akzeptiere.</Text>
         </View>
-        <Button title='Bestätigen'/>
+        <Button title='Bestätigen' color='red'/>
       </View>
     </View>
     /* View ist ein Container, nur eine View in einer Komponente, aber mehrere Unterviews/Views in der View */
