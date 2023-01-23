f62f14f7017a14d00def44664969df207cfa6955
sophieStritti <sophie.strittmatter@gmail.com>
2023-01-23 12:37:20 +0100

    added icons in navigation bar

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, Button, Switch, TextInput } from 'react-native'
 import React,{ useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
 
 import RalleyScreen from './js/pages/RalleyScreen';
 import SettingsScreen from './js/pages/SettingsScreen';
@@ -12,8 +14,23 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Ralley" component={RalleyScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'DHBW Campus Ralley') {
+              iconName = 'map';
+            } else if (route.name === 'Einstellungen') {
+              iconName = 'settings';
+            }
+
+            // You can return any component that you like here!
+            return <MaterialIcon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="DHBW Campus Ralley" component={RalleyScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
         <Tab.Screen name="Einstellungen" component={SettingsScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
       </Tab.Navigator>
     </NavigationContainer>
