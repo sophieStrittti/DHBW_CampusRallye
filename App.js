ae8aee240b8ec9214fed1d0aac02eb91226d5e4d
Fabian2206 <kaisefab@dhbw-loerrach.de>
2023-02-21 15:13:05 +0100

    Einstellungsseite mit klickbaren Feldern

--- App.js
+++ App.js
@@ -3,40 +3,56 @@ import React,{ useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-
+import { createStackNavigator } from '@react-navigation/stack';
+import { Screen } from 'react-native-screens';
 
 import RalleyScreen from './js/pages/RalleyScreen';
 import SettingsScreen from './js/pages/SettingsScreen';
+import ImpressumScreen from './js/pages/ImpressumScreen';
+import SpracheScreen from './js/pages/SpracheScreen';
+import InformationenScreen from './js/pages/InformationenScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 /* main App; uses the defined pages and navigates between them */
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'DHBW Campus Ralley') {
-              iconName = 'map';
-            } else if (route.name === 'Einstellungen') {
-              iconName = 'settings';
-            }
-
-            // You can return any component that you like here!
-            return <MaterialIcon name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}>
-        <Tab.Screen name="DHBW Campus Ralley" component={RalleyScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
-        <Tab.Screen name="Einstellungen" component={SettingsScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Ralley" component={TabScreen} />
+        <Stack.Screen name="Einstellungen" component={SettingsScreen} />
+        <Stack.Screen name="Impressum" component={ImpressumScreen} />
+        <Stack.Screen name="Sprache" component={SpracheScreen} />
+        <Stack.Screen name="Informationen" component={InformationenScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+function TabScreen() {
+  return (
+    <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'DHBW Campus Ralley') {
+            iconName = 'map';
+          } else if (route.name === 'Einstellungen') {
+            iconName = 'settings';
+          }
+
+          // You can return any component that you like here!
+          return <MaterialIcon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="DHBW Campus Ralley" component={RalleyScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
+      <Tab.Screen name="Einstellungen" component={SettingsScreen} options={{ headerStyle: {backgroundColor: '#f4f3f4'}}}/>
+    </Tab.Navigator>
+  );
+}
 
 /*-----------------------------------------------styles----------------------------------------------- */
 
@@ -82,5 +98,4 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     width: 300,
   }
-
 });
--- js/pages/SettingsScreen.js
+++ js/pages/SettingsScreen.js
@@ -1,13 +1,51 @@
-import { StyleSheet, Text, View, Button, Switch, TextInput } from 'react-native';
-import React,{ useState } from 'react';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-/* settings */
-export default function SettingsScreen (){
+// ...
 
-    /* page content */
-    return (
-      <View>
-        <Text>This is the Settings Screen</Text>
-      </View>
-    );
-  }
+export default function SettingsScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.tile} onPress={() => navigation.navigate('Impressum')}>
+        <Text style={styles.tileText}>Impressum</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.tile} onPress={() => navigation.navigate('Sprache')}>
+        <Text style={styles.tileText}>Sprache und Steuerung</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.tile} onPress={() => navigation.navigate('Informationen')}>
+        <Text style={styles.tileText}>Informationen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  tile: {
+    width: '80%',
+    height: 100,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'grey',
+  },
+
+  tileText: {
+    fontSize: 20,
+    color: 'grey',
+  },
+});
