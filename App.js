9ca289ed4a97bae4b7212f352b5b1f9bc4647c4d
sophieStritti <sophie.strittmatter@gmail.com>
2023-02-21 15:32:23 +0100

    fix

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React,{ useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Screen } from 'react-native-screens';
 
 import Color from './js/styles/Colors';
 
@@ -10,11 +12,31 @@ import Color from './js/styles/Colors';
 import RalleyScreen from './js/pages/RalleyScreen';
 import SettingsScreen from './js/pages/SettingsScreen';
 import GroupScreen from './js/pages/GroupScreen';
+import ImpressumScreen from './js/pages/ImpressumScreen';
+import SpracheScreen from './js/pages/SpracheScreen';
+import InformationenScreen from './js/pages/InformationenScreen';
+
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
 
 /* main App; uses the defined pages and navigates between them */
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Ralley" component={TabScreen} />
+        <Stack.Screen name="Einstellungen" component={SettingsScreen} />
+        <Stack.Screen name="Impressum" component={ImpressumScreen} />
+        <Stack.Screen name="Sprache" component={SpracheScreen} />
+        <Stack.Screen name="Informationen" component={InformationenScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function TabScreen() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={({ route }) => ({
@@ -28,8 +50,6 @@ export default function App() {
             } else if (route.name === 'Gruppe') {
               iconName = 'people';
             }
-
-            // You can return any component that you like here!
             return <MaterialIcon name={iconName} size={size} color={focused ? Color.dhbwRed : Color.dhbwGray}/>;
           },
           tabBarActiveTintColor: Color.dhbwRed,
