3eb1e9709b466e579f6371c226c76f6a54b1d097
sophieStritti <sophie.strittmatter@gmail.com>
2023-03-26 13:58:19 +0200

    added informations on InformationScreen

--- js/pages/InformationenScreen.js
+++ js/pages/InformationenScreen.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Linking, ScrollView, StyleSheet } from 'react-native';
+import Colors from '../styles/Colors';
 
 export default function InformationenScreen() {
   return (
-    <View>
-      <Text>Informationen Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>Die Idee zu dieser Campus Rallye App entstand als Idee der Managerin des Studienzentrums IT-Management und Informatik der DHBW Lörrach Ulrike Menke.
+          Die Konzeption und Umsetzung erfolgte an der DHBW Lörrach im Rahmen eines Projekts für die Studienarbeit des Studiengangs Informatik durch Studierende unter Betreuung und Leitung von Ulrike Menke (Konzeptgestaltung und Projektbetreuung) und Prof. Dr. Erik Behrends (technische Umsetzung).
+          {'\n'}
+          {'\n'}
+          Die Campus Rallye App soll kontinuierlich weiterentwickelt werden.
+          {'\n'}
+          {'\n'}
+          Die App ist ein Open Source Projekt: 
+        </Text>
+        <Text
+          style={{color: Colors.dhbwRed}}
+          onPress={() =>
+            Linking.openURL(
+              'https://github.com/sophieStrittti/DHBW_CampusRallyeApp'
+            )
+          }
+        >
+          https://github.com/sophieStrittti/DHBW_CampusRallyeApp
+        </Text>
+        <Text>
+        {'\n'}
+          Version (App): 1.0.0
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    padding: 15,
+  },
+   
+});
