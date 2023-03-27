bb0d5e17e003ce2ab96ef03cc66fbea94c05163c
sophieStrittti <75489421+sophieStrittti@users.noreply.github.com>
2023-03-27 04:13:23 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,9 +3,8 @@ import { createClient } from "@supabase/supabase-js"
 // Muss drin sein. Sonst gibt es einen URL Fehler, wenn man supabase an anderer Stelle importiert.
 import 'react-native-url-polyfill/auto'
 
-const supabaseUrl = "https://ytplawltiqcvsskzrxuo.supabase.co"
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0cGxhd2x0aXFjdnNza3pyeHVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk2Njc5MDUsImV4cCI6MTk5NTI0MzkwNX0.LSD06Jm6wFtYwlma9X5gmXIGSOjStFdFrWrTz57pp_Q"
+const supabaseUrl = "YOUR_URL"
+const supabaseAnonKey = "YOUR_ANON_KEY"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
